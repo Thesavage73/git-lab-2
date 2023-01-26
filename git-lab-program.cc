17e28b8917fd5a5649f176a24eb2b2396d8a61eb
Colton Savage <cs845221@ohio.edu>
2023-01-25 19:19:18 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Colton Savage>
+ *        Date: <1/25/2023>
+ * Description: reposository lab 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
